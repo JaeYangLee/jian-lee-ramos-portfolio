9ab9fcfe6825a51a95edf54ba9482625f683d0aa
Jian Lee <ramosjianlee@gmail.com>
2025-08-21 00:44:46 +0800

    feat: redesign MyPortfolioPageTwo with updated header and MyPortfolioProjectCard component

--- src/pages/MyPortfolioPageTwo.jsx
+++ src/pages/MyPortfolioPageTwo.jsx
@@ -1,51 +1,23 @@
 import React from "react";
-import ProjectDetailCard from "../components/ProjectDetailCard";
+import MyPortfolioProjectCard from "../components/MyPortfolioProjectCard";
 
 function MyPortfolioPageTwo() {
   return (
     <>
-      <div className="p-8 flex flex-col justify-center items-center w-screen min-h-screen font-antonio bg-[#F9EEE8]">
-        <div className="flex flex-col items-center justify-center gap-8 p-8">
-          <h1 className="text-center text-4xl md:text-6xl lg:text-7xl xl:text-8xl font-bold text-[#31302D]">
-            MY PROJECTS
+      <div className="w-full h-full bg-[#F9EEE8]">
+        <header className="flex flex-col gap-4 p-4 pt-12 md:px-12">
+          <h1 className="text-3xl font-bold text-center md:text-left font-antonio text-[#31302D]">
+            MY SOFTWARE DEVELOPMENT PROJECTS
           </h1>
-          <h3 className="font-mono text-xs text-center text-[#31302D] xl:text-sm">
-            Here are some of the most exciting software projects I’ve worked on
-            and contributed to as a developer.
+          <h3 className="hidden font-mono text-sm text-left md:flex">
+            “As a Computer Science graduate, here are some of the projects where
+            I got to bring my ideas to life, apply what I’ve learned, and have
+            fun building along the way!”
           </h3>
-        </div>
-        <div className="flex flex-col gap-4 lg:flex-row xl:flex-row text-[#31302D]">
-          <ProjectDetailCard
-            projectLogo="assets/plantitotitaLogo.svg"
-            projectName="PLANTITOTITA"
-            projectDescription="A plant scanning mobile application."
-            featureOne="‣ Augmented Reality Plant Recognition with detailed botanical Information."
-            featureTwo="‣ Augmented Reality Disease diagnosis with detailed botanical Information."
-            featureThree="‣ Informative Plant Care and Medical Guide."
-            techStackOne="• Java"
-            techStackTwo="• Android Studio"
-            techStackThree="• Java Firebase"
-            techStackFour="• Figma"
-            projectLink="https://github.com/JaeYangLee/PLANTITOTITA"
-          />
-          <ProjectDetailCard
-            projectLogo="assets/CGISLogo.png"
-            projectName="CGWCEISC INVENTORY SYSTEM"
-            projectDescription="A web based inventory management system for PCG’s Storage and Accountability Officer."
-            featureOne="‣ User Authentication and Roles."
-            featureTwo="‣ Real-Time Inventory Tracking."
-            featureThree="‣ Item Management."
-            featureFour="‣ Inventory Logs & History."
-            featureFive="‣ Search and Filter Options."
-            featureSix="‣ Inventory Reports Generation."
-            techStackOne="• React.js"
-            techStackTwo="• TailwindCSS"
-            techStackThree="• Node.js"
-            techStackFour="• MySQL"
-            techStackFive="• Figma"
-            projectLink="https://github.com/JaeYangLee/pcg-inventory-system"
-          />
-        </div>
+        </header>
+        <main className="py-4">
+          <MyPortfolioProjectCard />
+        </main>
       </div>
     </>
   );
