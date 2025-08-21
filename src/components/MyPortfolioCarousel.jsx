e21b482f50bdc35e4d0e8c6067d8ea1fcb22eec3
Jian Lee <ramosjianlee@gmail.com>
2025-08-21 12:18:42 +0800

    feat: update MyPortfolioCarousel to display MyPortfolioProjectCard in the first slide

--- src/components/MyPortfolioCarousel.jsx
+++ src/components/MyPortfolioCarousel.jsx
@@ -1,14 +1,15 @@
 import React from "react";
+import MyPortfolioProjectCard from "./MyPortfolioProjectCard";
 
 function MyPortfolioCarousel() {
   return (
     <>
       <div className="carousel w-full h-full">
-        <div id="slide1" className="carousel-item relative w-full">
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.webp"
-            className="w-full"
-          />
+        <div
+          id="slide1"
+          className="carousel-item relative w-full flex items-center justify-center"
+        >
+          <MyPortfolioProjectCard />
           <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
             <a href="#slide4" className="btn btn-circle">
               ❮
