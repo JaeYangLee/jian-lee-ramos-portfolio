aab1b7e61932e28600976a23d969ba44b54946df
Jian Lee <ramosjianlee@gmail.com>
2025-08-21 13:08:20 +0800

    refactor: simplify MyPortfolioCarousel structure and replace static images with MyPortfolioProjectCard components

--- src/components/MyPortfolioCarousel.jsx
+++ src/components/MyPortfolioCarousel.jsx
@@ -1,69 +1,75 @@
 import React from "react";
 import MyPortfolioProjectCard from "./MyPortfolioProjectCard";
 
-function MyPortfolioCarousel({ children: slides }) {
+function MyPortfolioCarousel() {
   return (
     <>
-      <div>
-        <div className="carousel w-full">
-          <div id="slide1" className="carousel-item relative w-full">
-            <img
-              src="https://img.daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.webp"
-              className="w-full"
-            />
-            <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-              <a href="#slide4" className="btn btn-circle">
-                ❮
-              </a>
-              <a href="#slide2" className="btn btn-circle">
-                ❯
-              </a>
-            </div>
-          </div>
-          <div id="slide2" className="carousel-item relative w-full">
-            <img
-              src="https://img.daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.webp"
-              className="w-full"
-            />
-            <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-              <a href="#slide1" className="btn btn-circle">
-                ❮
-              </a>
-              <a href="#slide3" className="btn btn-circle">
-                ❯
-              </a>
-            </div>
-          </div>
-          <div id="slide3" className="carousel-item relative w-full">
-            <img
-              src="https://img.daisyui.com/images/stock/photo-1414694762283-acccc27bca85.webp"
-              className="w-full"
-            />
-            <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-              <a href="#slide2" className="btn btn-circle">
-                ❮
-              </a>
-              <a href="#slide4" className="btn btn-circle">
-                ❯
-              </a>
-            </div>
-          </div>
-          <div id="slide4" className="carousel-item relative w-full">
-            <img
-              src="https://img.daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.webp"
-              className="w-full"
-            />
-            <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-              <a href="#slide3" className="btn btn-circle">
-                ❮
-              </a>
-              <a href="#slide1" className="btn btn-circle">
-                ❯
-              </a>
-            </div>
+      <main className="carousel w-full h-full">
+        <div
+          id="slide1"
+          className="carousel-item relative w-full  flex items-center justify-center"
+        >
+          <MyPortfolioProjectCard />
+          <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+            <a
+              href="#"
+              onClick={(e) => {
+                e.preventDefault();
+                document
+                  .getElementById("slide4")
+                  .scrollIntoView({ behavior: "smooth", block: "nearest" });
+              }}
+              className="btn btn-circle"
+            >
+              ❮
+            </a>
+            <a
+              href="#"
+              onClick={(e) => {
+                e.preventDefault();
+                document
+                  .getElementById("slide2")
+                  .scrollIntoView({ behavior: "smooth", block: "nearest" });
+              }}
+              className="btn btn-circle"
+            >
+              ❯
+            </a>
           </div>
         </div>
-      </div>
+        <div
+          id="slide2"
+          className="carousel-item relative w-full flex items-center justify-center"
+        >
+          <MyPortfolioProjectCard />
+          <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+            <a
+              href="#"
+              onClick={(e) => {
+                e.preventDefault();
+                document
+                  .getElementById("slide1")
+                  .scrollIntoView({ behavior: "smooth", block: "nearest" });
+              }}
+              className="btn btn-circle"
+            >
+              ❮
+            </a>
+            <a
+              href="#"
+              onClick={(e) => {
+                e.preventDefault();
+                document
+                  .getElementById("slide3")
+                  .scrollIntoView({ behavior: "smooth", block: "nearest" });
+              }}
+              className="hidden btn btn-circle"
+            >
+              ❯
+            </a>
+          </div>
+        </div>
+      </main>
     </>
   );
 }
