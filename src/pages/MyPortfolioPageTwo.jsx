1c9c723371a13e13d201bb9ea34dd0f841323003
Jian Lee <ramosjianlee@gmail.com>
2025-08-21 14:32:04 +0800

    style: improve layout and typography in MyPortfolioPageTwo for better readability

--- src/pages/MyPortfolioPageTwo.jsx
+++ src/pages/MyPortfolioPageTwo.jsx
@@ -7,11 +7,11 @@ function MyPortfolioPageTwo() {
   return (
     <>
       <div className="w-full h-full bg-[#F9EEE8]">
-        <header className="flex flex-col gap-4 p-4 pt-12 md:px-24">
-          <h1 className="text-3xl md:text-4xl font-bold text-center md:text-left font-antonio text-[#31302D]">
+        <header className="flex flex-col gap-4 p-8 pt-12 md:px-24 lg:px-36 xl:px-68">
+          <h1 className="text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-bold text-center md:text-left font-antonio text-[#31302D]">
             MY SOFTWARE DEVELOPMENT PROJECTS
           </h1>
-          <h3 className="hidden font-mono text-xs text-justify md:flex text-[#31302D]">
+          <h3 className="hidden font-mono text-xs lg:text-sm xl:text-lg text-justify md:flex text-[#31302D] lg:pr-40">
             As a Computer Science graduate, here are some of the projects where
             I got to bring my ideas to life, apply what I’ve learned, and have
             fun building along the way!
